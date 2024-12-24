93570e36f4b37facda97c821f8d994934eb30aee
senagab <senagab@gmail.com>
2024-12-23 23:20:00 -0400

    feat: estilização via modulo

--- src/components/Perfil/index.jsx
+++ src/components/Perfil/index.jsx
@@ -5,7 +5,7 @@ const Perfil = ({ nomeUsuario }) => {
     return (
         <header className={styles.header}>
             <img className={styles.avatar} src={`https://github.com/${nomeUsuario}.png`} alt="" />
-            <h1 className="perfil-titulo">
+            <h1 className={styles.name}>
                 {nomeUsuario}
             </h1>
         </header>
--- src/components/ReposList/ReposListIndex.jsx
+++ src/components/ReposList/ReposListIndex.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 
+import styles from './ReposList.module.css';
+
 const ReposList = () => {
     const [repos, setRepos] = useState([]);
     const [estaCarregando, setEstaCarregando] = useState(true);
@@ -17,21 +19,26 @@ const ReposList = () => {
     }, []);
 
     return (
-        <>
+        <div className="container">
             {estaCarregando && (
                 <h1>Carregando...</h1>
             )}
-            <ul>
+            <ul className={styles.list}>
                 {/* {repos.map(repositorio => ( */}
                 {repos.map(({id, name, language, html_url}) => (
-                    <li key={id}>
-                        <b>Nome:</b> {name} <br />
-                        <b>Linguagem:</b> {language} <br />
-                        <a target="_blank" href={html_url}>Visitar no Github</a> 
+                    <li className={styles.listItem}  key={id}>
+                        <div className={styles.itemName}>
+                            <b>Nome:</b> {name} <br />
+                        </div>
+                        <div className={styles.itemLanguage}>
+                            <b>Linguagem:</b> 
+                            {language} <br/>
+                        </div>
+                        <a className={styles.itemLink} target="_blank" href={html_url}>Visitar no Github</a> 
                     </li>
                 ))}
             </ul>
-        </>
+        </div>
     )
 }
 
